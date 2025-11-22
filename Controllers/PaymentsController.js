e42422041a923b7f2075fa86a80cf94238d1ec2c
ThammanaDheeraj <thadhee2005@gmail.com>
2025-11-22 16:18:27 +0530

    UPI added

--- Controllers/PaymentsController.js
+++ Controllers/PaymentsController.js
@@ -63,7 +63,7 @@ const CreateAccount=async (req,res)=>{
 
 const SendMoney=async(req,res)=>{
     const userAccount=req.account;
-    const receiver=req.params.recAccNum;
+    const receiver=req.body.accountNumber;
     const money=Number(req.body.money);
     if(!money||money<=0){
         return res.status(400).json({message:"Invalid Amount"})
@@ -141,9 +141,83 @@ const Transhistory=async (req,res)=>{
     }
 }
 
+const UPI=async (req,res)=>{
+    const userAccount=req.account;
+    const userph=req.account.phonenumber;
+    const {phonenumber,money}=req.body;
+    if(!phonenumber || !money ){
+        return res.status(400).json({
+            message:"All fields are required",
+            success:false
+        })
+    }
+    try{
+        const receiver=await Account.findOne({phonenumber:phonenumber,primary:true});
+        if(!receiver){
+            return res.status(400).json({
+                message:"Receiver doesn't have UPI",
+                success:false
+            })
+        }
+        if(money<=0){
+            return res.status(400).json({
+                message:"Enter a Valid Amount to be transfered",
+                success:false
+            })
+        }
+         if(money>userAccount.balance){
+            const trans=new TransactionHistory({
+                senderId:userAccount.accountNumber,
+                receiverId:receiver.accountNumber,
+                money:money,
+                status:"Failed"
+            })
+            await trans.save();
+            return res.status(400).json({message:"Insufficient Balance",success:false})
+        }
+        const session=await mongoose.startSession();
+        session.startTransaction();
+        try{
+            await Account.updateOne(
+                {phonenumber:userph},
+                {$inc:{balance:-money}},{session}
+            );
+            await Account.updateOne(
+                {phonenumber},
+                {$inc:{balance:money}},{session}
+            );
+            const trans2=new TransactionHistory({
+                senderId:userAccount.phonenumber,
+                receiverId:receiver.phonenumber,
+                money:money,status:"Success"}
+            );
+            await trans2.save({session});
+            await session.commitTransaction();
+            session.endSession();
+            return res.status(200).json({
+                message:"Money Sent Successfully",
+                success:true
+            });
+        }catch(err){
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(500).json({
+                message:"Transaction Failed",
+                success:false
+            });
+        }
+    }catch(err){
+        return res.status(500).json({
+            message:"Internal Sever Error",
+            success:false
+        })
+    }
+}
+
 module.exports={
     SendMoney,
     CheckBalance,
     CreateAccount,
-    Transhistory
+    Transhistory,
+    UPI
 }
--- Routes/PaymentRoutes.js
+++ Routes/PaymentRoutes.js
@@ -1,12 +1,13 @@
 const EnsureAccount=require('../MiddleWares/EnsureAccount');
 const Auth=require('../MiddleWares/Auth');
-const { CreateAccount, CheckBalance, SendMoney, Transhistory } = require('../Controllers/PaymentsController');
+const { CreateAccount, CheckBalance, SendMoney, Transhistory, UPI } = require('../Controllers/PaymentsController');
 const router=require('express').Router();
 
 router.use(Auth);
 router.post('/createaccount',CreateAccount);
 router.get('/checkbalance',EnsureAccount,CheckBalance);
-router.post('/sendmoney/:recAccNum',EnsureAccount,SendMoney);
+router.post('/sendmoney',EnsureAccount,SendMoney);
 router.get('/history',EnsureAccount,Transhistory);
+router.post('/upi',EnsureAccount,UPI);
 
 module.exports=router;
